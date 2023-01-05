816abbf7f32513e9e018dc4f7dc5655ad48dd825
bagasys <bagasys3@gmail.com>
2023-01-05 19:30:55 +0700

    fix migration file: remove unique constraint from merchant_name

--- migrations/001_create_merchants_table.ts
+++ migrations/001_create_merchants_table.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 export async function up(knex: Knex) {
   await knex.schema.createTable("merchants", (table) => {
     table.increments("id");
-    table.string("merchant_name").notNullable().unique();
+    table.string("merchant_name").notNullable();
     table.string("phone_number").notNullable();
     table.decimal("latitude", 11, 8).notNullable();
     table.decimal("longitude", 11, 8).notNullable();
